fd015f5c04c7b7c1712536381db41d4dc6d5dc44
Anna Langner <annalangner06@gmail.com>
2020-06-09 11:21:56 +0200

    Initiating the project with ready configuration of the webpack and babel enviroment

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// const person = require('./mymodule1');
+
+import {person} from './mymodule2';
+console.log(person.name);
+
+const greeting = 'Hello World';
+console.log(greeting);
+
+const getData = async (url) => {
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+};
+
+getData('https://jsonplaceholder.typicode.com/posts');
